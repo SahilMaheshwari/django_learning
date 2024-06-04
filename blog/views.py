d898d253352fa28fa6f8c39c0bf16eb3f4a5d208
Sahil <sahil7503@gmail.com>
2024-06-04 14:50:41 +0530

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	new file:   blog/__pycache__/__init__.cpython-311.pyc
    	new file:   blog/__pycache__/admin.cpython-311.pyc
    	new file:   blog/__pycache__/apps.cpython-311.pyc
    	new file:   blog/__pycache__/models.cpython-311.pyc
    	new file:   blog/__pycache__/urls.cpython-311.pyc
    	new file:   blog/__pycache__/views.cpython-311.pyc
    	new file:   blog/migrations/__pycache__/__init__.cpython-311.pyc
    	new file:   blog/static/blog/main.css
    	new file:   blog/templates/blog/about.html
    	new file:   blog/templates/blog/base.html
    	new file:   blog/templates/blog/home.html
    	new file:   blog/urls.py
    	modified:   blog/views.py
    	modified:   django_learning/__pycache__/settings.cpython-311.pyc
    	modified:   django_learning/__pycache__/urls.cpython-311.pyc
    	modified:   django_learning/settings.py
    	modified:   django_learning/urls.py

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author' : 'Mahil Saheshwari',
+        'title'  : 'Make online ps please',
+        'content': 'Pleleeeazzeeeee',
+        'date'   : '7 may 2010'
+    },
+    {
+        'author' : 'Drums mAN',
+        'title'  : 'I love drums man',
+        'content': 'Ba dum tss',
+        'date'   : '31 Aug 2015'
+    }
+]
 
-# Create your views here.
+
+def home(request):
+    context = {
+        'posts' : posts
+        }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title' : 'About'})
